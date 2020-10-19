8533d885b7b0b1061c0f2cc78748c6c5168b6828
yassine issifi <yassineissifi@gmail.com>
2020-10-19 21:32:48 +0100

    add more functionalyti to the game

--- script.js
+++ script.js
@@ -16,46 +16,75 @@ function computerPlay() {
 // return the result of playRound
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
-    // this line is for debugin
-     console.log("= = = = = ="+playerSelection);
         // if player chos rock
     if (playerSelection === 'ROCK') {
         if (computerSelection === 'PAPER') {
-            alert ('you lost. rock > paper')
+            document.getElementById("userScore").innerHTML = " 0 ";
+            document.getElementById("computerScore").innerHTML = " 1 ";
+            document.getElementById("result").innerHTML = "You Lose! Paper beats Rock";
+            // alert ('you lost. rock > paper')
         } else if (computerSelection === 'SCISSORS') {
-            alert ('you wins. rock > scissors')
+            document.getElementById("userScore").innerHTML = " 1 ";
+            document.getElementById("computerScore").innerHTML = " 0 ";
+            document.getElementById("result").innerHTML = "You Win! Rock beats Scissors";
+            // alert ('you wins. rock > scissors')
         }else if (computerSelection == 'ROCK') {
-            alert ('egal ')
+            document.getElementById("userScore").innerHTML = " 0 ";
+            document.getElementById("computerScore").innerHTML = " 0 ";
+            document.getElementById("result").innerHTML = " STRICK ";
+            // alert ('egal ')
         }
         // in case of player chos paper
     } else if (playerSelection === 'PAPER') {
 
         if (computerSelection === 'PAPER') {
-            alert ('egal')
+            document.getElementById("userScore").innerHTML = " 0 ";
+            document.getElementById("computerScore").innerHTML = " 0 ";
+            document.getElementById("result").innerHTML = " STRICK "
+            // alert ('egal')
         } else if (computerSelection === 'SCISSORS') {
-            alert ('you lost. paper < scissors')
+            document.getElementById("userScore").innerHTML = " 0 ";
+            document.getElementById("computerScore").innerHTML = " 1 ";
+            document.getElementById("result").innerHTML = "You Lose! Scissors beat Paper"
+            // alert ('you lost. paper < scissors')
         }else if (computerSelection == 'ROCK') {
-            alert ('you wins. paper > rock ')
+            document.getElementById("userScore").innerHTML = " 1 ";
+            document.getElementById("computerScore").innerHTML = " 0 ";
+            document.getElementById("result").innerHTML = "You Wine! Paper beat Rock"
+            // alert ('you wins. paper > rock ')
         }
         // in case of player chos scissors
     } else if (playerSelection === 'SCISSORS') {
         if (computerSelection === 'PAPER') {
-            alert ('you wins. scissors > paper ')
+            document.getElementById("userScore").innerHTML = " 1 ";
+            document.getElementById("computerScore").innerHTML = " 0 ";
+            document.getElementById("result").innerHTML = "You Wine! Scissors beat Paper";
+            // alert ('you wins. scissors > paper ')
         } else if (computerSelection === 'SCISSORS') {
-            alert ('egal')
+            document.getElementById("userScore").innerHTML = " 0 ";
+            document.getElementById("computerScore").innerHTML = " 0 ";
+            document.getElementById("result").innerHTML = " STRICK "; 
+            // alert ('egal')
         }else if (computerSelection == 'ROCK') {
-            alert( 'you lost. scissors < rock')
+            document.getElementById("userScore").innerHTML = " 0 ";
+            document.getElementById("computerScore").innerHTML = " 1 ";
+            document.getElementById("result").innerHTML = "You Lose! Rock beat Scissors";
+            //alert( 'you lost. scissors < rock')
         }
     }
 }
-// triger function
-function playGame() {
-    // loop 5 time
-    for (let index = 0; index < 5; index++) {
-        playerSelection = prompt('enter a chose')
-        playRound(playerSelection,computerPlay() )
-    }
+// get player selection
+function playerSelection(ps){
+    playRound(ps,computerPlay())
 }
+// triger function
+// function playGame() {
+    // loop 5 time
+    // for (let index = 0; index < 5; index++) {
+        // playerSelection = prompt('enter a chose')
+        // playRound(playerSelection,computerPlay() )
+    // }
+// }
 // set the function on the button
 
 
